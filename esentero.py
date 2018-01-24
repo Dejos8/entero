9d4983761499eaf3d30d34f701d66801c07e3ac2
ism3769892 <ism3769892@a18.informatica.escoladeltreball.org>
2018-01-24 12:00:08 +0100

    second blog

---
+++ esentero.py
@@ -0,0 +1,9 @@
+#coding: utf­8
+print("DIVISIÓN")
+dividendo = int(input("Escriba el dividendo: "))
+divisor = int(input("Escriba el divisor: "))
+
+if dividendo % divisor == 0:
+    print("es entero")
+else:
+    print("no es entero")
